bfa21423e50ae1de6328d11c20d11b86a685af5d
Pravin <pravinlearning@zoho.com>
2022-11-28 22:07:09 +0000

    includes the changes

--- POMSeries/src/main/java/com/qa/opencart/pages/POC.java
+++ POMSeries/src/main/java/com/qa/opencart/pages/POC.java
@@ -3,12 +3,8 @@ package com.qa.opencart.pages;
 public class POC {
 
 	public static void main(String[] args) {
-		 int i = 10;
-		 int h = 20;
-		 int sum = i+h;
-    
-    String name = "poc";
-		 
+		String name = "poc";
+		String id = "122";
 	}
 
 }
