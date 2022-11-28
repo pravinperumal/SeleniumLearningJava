14373b32d704ea1b43a9217bcd6810b8dc584dec
pravinperumal <pravinlearning@zoho.com>
2022-11-28 21:42:57 +0000

    Create POC.java

---
+++ POMSeries/src/main/java/com/qa/opencart/pages/POC.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class POC {
+
+	public static void main(String[] args) {
+		 int i = 10;
+		 int h = 20;
+		 int sum = i+h;
+    
+    String name = "poc";
+		 
+	}
+
+}
